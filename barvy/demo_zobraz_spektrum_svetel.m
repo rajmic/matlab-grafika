08ea44cffcf8f0010e86f6c680250232c02c00b3
Pavel Rajmic <rajmic@users.noreply.github.com>
2020-02-12 10:06:56 +0100

    Přidáno vykreslování spektra obyč. žárovky

--- barvy/demo_zobraz_spektrum_svetel.m
+++ barvy/demo_zobraz_spektrum_svetel.m
@@ -6,32 +6,39 @@
 clc
 close all
 
-%% spektrum
-[wl, psd] = zmerena_spektra_zdroju();
+%% nacteni spekter
+[wl1, psd1] = zmerena_spektra_zdroju();
 
 %oriznuti na viditelne
-crop = (wl>=380 & wl<=780);
-wl = wl(crop,:);
-psd = psd(crop,:);
+crop = (wl1>=380 & wl1<=780);
+wl1 = wl1(crop,:);
+psd1 = psd1(crop,:);
 
+%zarovka
+[wl2, psd2] = spektrum_zarovky();
 
 %% vykresleni
-% OSRAM
-plot_color_power_spectral_density(wl, psd(:,1), false)
+% OSRAM LED
+plot_color_power_spectral_density(wl1, psd1(:,1), false)
 title('')
 ylabel('Vykon [W/nm]')
 
 % Hynek Medricky -- mod 1 (http://www.design-light.cz/den-bulb.html)
-plot_color_power_spectral_density(wl, psd(:,2), false)
+plot_color_power_spectral_density(wl1, psd1(:,2), false)
 title('')
 ylabel('Vykon [W/nm]')
 
 % Hynek Medricky -- mod 2 (http://www.design-light.cz/den-bulb.html)
-plot_color_power_spectral_density(wl, psd(:,3), false)
+plot_color_power_spectral_density(wl1, psd1(:,3), false)
 title('')
 ylabel('Vykon [W/nm]')
 
 % Hynek Medricky -- mod 3 (http://www.design-light.cz/den-bulb.html)
-plot_color_power_spectral_density(wl, psd(:,4), false)
+plot_color_power_spectral_density(wl1, psd1(:,4), false)
 title('')
 ylabel('Vykon [W/nm]')
+
+% Zarovka obycejna
+plot_color_power_spectral_density(wl2, psd2, false)
+title('')
+ylabel('Normalizovany vykon [W/nm]')
